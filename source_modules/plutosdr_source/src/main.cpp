b80aa03f927cc50d74aed9a9b7e8ea245467fc88
Primoz Beltram <primoz.beltram@protonmail.com>
2026-02-02 13:10:17 +0100

    Remove redundant code from plutosdr_source.

--- source_modules/plutosdr_source/src/main.cpp
+++ source_modules/plutosdr_source/src/main.cpp
@@ -259,11 +259,6 @@ private:
 
             int16_t* buf = (int16_t*)iio_buffer_first(rxbuf, rx0_i);
 
-            for (int i = 0; i < blockSize; i++) {
-                _this->stream.writeBuf[i].re = (float)buf[i * 2] / 32768.0f;
-                _this->stream.writeBuf[i].im = (float)buf[(i * 2) + 1] / 32768.0f;
-            }
-
             volk_16i_s32f_convert_32f((float*)_this->stream.writeBuf, buf, 32768.0f, blockSize*2);
 
             if (!_this->stream.swap(blockSize)) { break; };
